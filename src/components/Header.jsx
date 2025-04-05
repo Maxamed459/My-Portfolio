dac1a3725ac911fa77720365e12c114e5b978829
Maxamed459 <maxamedmahdi459@gmail.com>
2025-04-05 15:24:46 +0300

    modified something

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,7 +35,7 @@ const Header = () => {
                 </div>
                 <button className="hidden lg:flex items-center gap-2 border border-gray-300 hover:border-gray-500 px-6 py-2 rounded-lg">
                     <i className="fa-solid fa-right-to-bracket text-[#3955E5]"></i>
-                    <span>Get Started</span>
+                    <span>Hire Me</span>
                 </button>
 
                 {/* Mobile Menu Button */}
@@ -45,7 +45,7 @@ const Header = () => {
 
                 {/* Mobile Navigation */}
                 {isNavOpen && (
-                    <div className="fixed z-10 lg:hidden inset-0 p-3 bg-white">
+                    <div className="fixed z-10 lg:hidden inset-0 py-3 px-7 bg-white">
                         <div className="flex justify-between items-center">
                             <Link to="/" className="flex items-center gap-2">
                                 <img src={Logo} className="h-10 w-auto" alt="logo" />
@@ -70,7 +70,7 @@ const Header = () => {
                         <div className="h-[1px] bg-gray-300"></div>
                         <button className="w-full flex mt-6 items-center gap-2 hover:bg-gray-50 px-6 py-2 rounded-lg">
                             <i className="fa-solid fa-right-to-bracket text-[#3955E5]"></i>
-                            <span>Login</span>
+                            <span>Hire Me</span>
                         </button>
                     </div>
                 )}
