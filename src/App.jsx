3b09507542c377c4a0b88fe56712eff8153281ac
Maxamed459 <maxamedmahdi459@gmail.com>
2025-06-23 13:14:01 +0300

    add some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./components/pages/Home";
-import About from "./components/pages/About";
-import Portfolio from "./components/pages/Portfolio";
-import Services from "./components/pages/Services";
-import Contacts from "./components/pages/Contacts";
+import Portfolio from "./pages/Portfolio";
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/portfolio" element={<Portfolio />}></Route>
-        <Route path="/services" element={<Services />}></Route>
-        <Route path="/Contacts" element={<Contacts />}></Route>
+        <Route path="/" element={<Portfolio />}></Route>
       </Routes>
     </>
   );
