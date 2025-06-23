b7477d9d2e0929ca29931404b8912697f736baeb
Maxamed459 <maxamedmahdi459@gmail.com>
2025-06-23 13:16:45 +0300

    add Contacts component

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Hero from "./Hero";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <h1>Welcome to contacts page</h1>
+    </>
+  );
+};
+
+export default Home;
