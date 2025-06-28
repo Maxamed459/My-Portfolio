907469b62356949248217bbe2272e0b34929c853
Maxamed459 <maxamedmahdi459@gmail.com>
2025-06-28 10:59:57 +0300

    modified something

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const Home = () => {
       id="about"
       className="flex flex-col lg:flex-row justify-center py-20 gap-5 container mx-auto"
     >
-      <div className="max-w-[90%] lg:w-1/3 flex flex-col items-center rounded-[10px] p-9 border-[1px] border-slate-200">
+      <div className="max-w-[90%] mx-auto lg:w-1/3 flex flex-col items-center rounded-[10px] p-9 border-[1px] border-slate-200">
         <img src={Me} className="w-full rounded-md" alt="my image" />
         <div className="mt-4 space-y-1 text-center">
           <h3 className="font-sync text-xl uppercase font-[500]">
